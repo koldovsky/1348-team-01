a516c6122157c1e9e02221fbb360628c056c98fc
Vitaliiliah <23liakh.v@nltu.lviv.ua>
2025-02-19 01:00:57 +0200

    realized the task using a java script

--- codewars-tasks/vitalik/Keep_Hydrated.js
+++ codewars-tasks/vitalik/Keep_Hydrated.js
@@ -1,1 +1,4 @@
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+function litres(time) {
+    return Math.floor(time * 0.5);
+  }
--- codewars-tasks/vitalik/Messi_Goals.js
+++ codewars-tasks/vitalik/Messi_Goals.js
@@ -1,1 +1,4 @@
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals(laLiga, copaDelRey, champions) {
+    return laLiga + copaDelRey + champions;
+}
