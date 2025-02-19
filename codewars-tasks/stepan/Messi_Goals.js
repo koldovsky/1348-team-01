8819362924b79025c22d2d13e352988b3ae4cb07
Yuliia Ivaniuk <ivanykjula@gmail.com>
2025-02-19 16:57:44 +0200

    Choose solutions with team

--- codewars-tasks/stepan/Messi_Goals.js
+++ codewars-tasks/stepan/Messi_Goals.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-  }
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
 
-  //VISCA BARCA
+//VISCA BARCA
--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,15 +1,37 @@
-// https://www.codewars.com/kata/56214b6864fe8813f1000019/train/javascript
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-const health = 100;
-const position = 0;
-const coins = 0;
-
-function main() {
-  rollDice();
-  move();
-  combat();
-  getCoins();
-  buyHealth();
-  printStatus();
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
+//VISCA BARCA :)
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  return position + roll * 2;
+}
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+  return (time / 2) >> 0;
+}
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
