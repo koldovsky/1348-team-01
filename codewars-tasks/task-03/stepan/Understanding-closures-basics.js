72df1f8a6ee3ef722345e29301e23af33be88426
Stepan <gamkivstepan@gmail.com>
2025-02-23 20:39:42 +0200

    do 3/4 tasks

--- codewars-tasks/task-03/stepan/Understanding-closures-basics.js
+++ codewars-tasks/task-03/stepan/Understanding-closures-basics.js
@@ -1,1 +1,11 @@
 //https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n){
+
+	let result = []
+
+	for (let i = 0; i< n; i++){
+		result.push(()=>i);
+	}
+	return result;
+}
