1ec95d567d346c0e76d671747a6ab2381d850e51
Yuliia Ivaniuk <ivanykjula@gmail.com>
2025-02-24 16:59:27 +0200

    Added final solution

--- codewars-tasks/task-03/yuliia/Understanding-closures-basics.js
+++ codewars-tasks/task-03/yuliia/Understanding-closures-basics.js
@@ -1,1 +1,16 @@
 //https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n) {
+    var res = [];
+
+    for (var i = 0; i < n; i++) {
+        res.push(
+            (function (x) {
+                return function () {
+                    return x;
+                };
+            })(i)
+        );
+    }
+    return res;
+}
