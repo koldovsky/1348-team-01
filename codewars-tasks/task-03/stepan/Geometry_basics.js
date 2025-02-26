a30a3b85f9199e2d1a359144c15b7d102b56ea98
Stepan <gamkivstepan@gmail.com>
2025-02-26 15:06:39 +0200

    add index.exclusive-offers.partial.html

--- codewars-tasks/task-03/stepan/Geometry_basics.js
+++ codewars-tasks/task-03/stepan/Geometry_basics.js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript 
 
 function circleCircumference(circle) {
-    return +(2 * 3.141592653589793 * circle.radius);
+    return (2 * 3.141592653589793 * circle.radius);
 }
