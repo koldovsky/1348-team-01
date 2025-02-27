10da97eb5a7f4452f3a65abe3d99886f23b10143
Vitaliiliah <23liakh.v@nltu.lviv.ua>
2025-02-27 07:27:23 +0200

    updated shop.store-products section and shop_products.js file with pagination

--- js/shop_products.js
+++ js/shop_products.js
@@ -1,44 +1,46 @@
-let currentPage = 0; 
-const pageSizes = [15, 5]; 
-let allProducts = []; 
-
+let currentPage = 0;
+const pageSizes = [20, 3];
+let allProducts = [];
 
 function startShop() {
-  allProducts = document.querySelectorAll(".store-products__item"); 
+  allProducts = document.querySelectorAll(".store-products__item");
   if (allProducts.length === 0) {
     console.log("Error!");
     return;
   }
+
+  let pageInUrl = window.location.search;
+  if (pageInUrl === "?page=1") currentPage = 0;
+  if (pageInUrl === "?page=2") currentPage = 1;
   showProducts();
 }
 
-
 function showProducts() {
-  let start = 0; 
+  let start = 0;
   if (currentPage > 0) {
     for (let i = 0; i < currentPage; i++) {
-      start += pageSizes[i]; 
+      start += pageSizes[i];
     }
   }
-
-  let end = start + pageSizes[currentPage]; 
+  let end = start + pageSizes[currentPage];
 
   allProducts.forEach((product, number) => {
     if (number >= start && number < end) {
-      product.style.display = "block"; 
+      product.style.display = "block";
     } else {
       product.style.display = "none";
     }
   });
 
- 
   document.querySelectorAll(".page-btn").forEach((button, index) => {
-    const pageIndex = index - 1;
+    let pageIndex = index - 1;
     if (button.textContent === "<" || button.textContent === ">") return;
     button.classList.toggle("active", pageIndex === currentPage);
   });
-}
 
+  if (currentPage === 0) window.history.pushState({}, "", "?page=1");
+  if (currentPage === 1) window.history.pushState({}, "", "?page=2");
+}
 
 function goToPage(newPage) {
   if (newPage >= 0 && newPage < pageSizes.length) {
@@ -47,15 +49,14 @@ function goToPage(newPage) {
   }
 }
 
-
 document.querySelectorAll(".page-btn").forEach((button) => {
   button.addEventListener("click", () => {
     if (button.textContent === "<") {
-      goToPage(currentPage - 1); 
+      goToPage(currentPage - 1);
     } else if (button.textContent === ">") {
-      goToPage(currentPage + 1); 
+      goToPage(currentPage + 1);
     } else {
-      goToPage(Number(button.textContent) - 1); 
+      goToPage(Number(button.textContent) - 1);
     }
   });
 });
