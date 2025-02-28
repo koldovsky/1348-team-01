dce3e116422bc12bbc2af46931b9a3f83edac93e
Lubomyr021 <lubomyr.bishko@gmail.com>
2025-02-28 13:31:38 +0200

    added carousel in about us our story

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 function init() {
-    import('./header_burger-menu.js');
-    import('./shop_products.js');
-    import('./countdown.js');
+  import("./header_burger-menu.js");
+  import("./shop_products.js");
+  import("./countdown.js");
+  import("./about_us.our-story-carousel.js");
 }
 
-const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener('htmx:afterOnLoad', () => {
-    loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) init();
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
 });
