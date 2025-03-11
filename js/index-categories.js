e78cca69f7da78340e5db425b31dfe152db4951f
Yuliia Ivaniuk <ivanykjula@gmail.com>
2025-03-11 05:59:33 +0200

    Finished filter-categories section

--- js/index-categories.js
+++ js/index-categories.js
@@ -1,6 +1,7 @@
 import { renderProducts, fetchProducts, getLastProductId } from "./render-products.js";
 const products = await fetchProducts();
 const categories = getProductsCategories(products);
+const navbarContainer = document.querySelector(".filters__navbar") //.filters__navbar
 
 const categoryProperties = {
     "mouse": {
@@ -11,30 +12,27 @@ const categoryProperties = {
     },
     "monitor": {
         "diagonal": [],
-        "panelType": [],
-        "responseTime": [],
-        "refreshRate": [],
+        "panel_type": [],
+        "response_time": [],
+        "refresh_rate": [],
         "resolution": [],
         "color": []
     },
     "keyboard": {
         "type": [],
         "switches": [],
-        "specialFeature": [],
+        "special_feature": [],
         "color": []
     },
     "headset": {
         "sensitivity": [],
         "impedance": [],
-        "frequencyRange": [],
+        "frequency_range": [],
         "color": []
     }
 }
 
-renderProducts(".categories__items", 12, true, [], [], [], false, [], 0)
-loadAdditionProducts();
-
-
+// Downloading addition items
 function loadAdditionProducts() {
     const additionContainer = document.querySelector(".addition-items");
     const btnLoadMore = document.querySelector(".categories__items-btn button");
@@ -58,48 +56,9 @@ function getProductsCategories(products) {
     return result
 }
 
-
-// function displayProductsCategories(categories, destination) {
-//     const productsContainer = document.querySelector(".categories__items")
-//     productsContainer.innerHTML = "";
-//     const wrapper = document.querySelector(destination)
-//     let innerContent = "<li><button>All</button></li>";
-//     categories.forEach(category => {
-//         innerContent += `<li><button>${category[0].toUpperCase() + category.slice(1)}</button></li>`
-//     })
-//     if (innerContent) {
-//         wrapper.innerHTML = innerContent;
-//         wrapper.querySelectorAll('button').forEach(element => {
-//             element.addEventListener("click", () => {
-//                 const loadBtnContainer = document.querySelector(".categories__items-container");
-//                 const category = element.textContent.toLowerCase();
-
-//                 if (category.toLowerCase() === "all") {
-//                     document.querySelector(".categories__items").innerHTML = ""
-//                     document.querySelector(".addition-items").innerHTML = ""
-//                     renderProducts(".categories__items", 12, true, [], [], [], true, [], 0);
-//                     loadBtnContainer.classList.add("load-btn")
-//                     loadAdditionProducts();
-
-//                 } else {
-//                     document.querySelector(".categories__items").innerHTML = ""
-//                     document.querySelector(".addition-items").innerHTML = ""
-//                     renderProducts(".categories__items", 12, true, [], [], [category], true, [], 0);
-//                     loadBtnContainer.classList.remove("load-btn")
-//                 }
-
-
-//             })
-//         })
-//     }
-// }
-
-
-function displayProductOptions(categoryProperties, destination) {
-    const navbarContainer = document.querySelector(destination) //.filters__navbar
-    const optionsContainer = document.querySelector(".navbar-item-options")
-    let innerHtmlContainer = "";
-
+// Display all categories list and render products according clicked filters 
+function displayProductsCategories(categoryProperties) {
+    // Fill oblect properties
     for (const product of products) {
         const category = product.category;
 
@@ -118,8 +77,6 @@ function displayProductOptions(categoryProperties, destination) {
         }
     }
 
-
-
     // ------------------------------------------------------------------------------
     const productsContainer = document.querySelector(".categories__items")
     productsContainer.innerHTML = "";
@@ -130,78 +87,169 @@ function displayProductOptions(categoryProperties, destination) {
     })
     if (innerContent) {
         wrapper.innerHTML = innerContent;
-        wrapper.querySelectorAll('button').forEach(element => {
-            element.addEventListener("click", () => {
+        const buttons = wrapper.querySelectorAll('button')
+        buttons.forEach(btn => {
+
+            btn.addEventListener("click", () => {
                 const loadBtnContainer = document.querySelector(".categories__items-container");
-                const category = element.textContent.toLowerCase();
+                const category = btn.textContent.toLowerCase();
+                buttons.forEach(button => button.classList.remove("selected"));
+                btn.classList.add("selected")
 
                 if (category.toLowerCase() === "all") {
                     document.querySelector(".categories__items").innerHTML = ""
                     document.querySelector(".addition-items").innerHTML = ""
+                    while (navbarContainer.children.length > 1) {
+                        navbarContainer.removeChild(navbarContainer.lastChild);
+                    }
                     renderProducts(".categories__items", 12, true, [], [], [], true, [], 0);
                     loadBtnContainer.classList.add("load-btn")
                     loadAdditionProducts();
-
+                    
 
                 } else {
                     document.querySelector(".categories__items").innerHTML = ""
                     document.querySelector(".addition-items").innerHTML = ""
                     renderProducts(".categories__items", 12, true, [], [], [category], true, [], 0);
                     loadBtnContainer.classList.remove("load-btn")
-                    
+                    while (navbarContainer.children.length > 1) {
+                        navbarContainer.removeChild(navbarContainer.lastChild);
+                    }
+                    displayAllFilters(category)
+                    renderProductsThroughFilters(category)
                 }
-
-
             })
+
+        
         })
     }
     // ------------------------------------------------------------------------------
-
-
-
-    for (const category in categoryProperties){
-        console.log(category)
-        innerHtmlContainer += `
-        <div class="navbar-item">
-            <div class="navbar-item__header">
-                <div class="navbar-item__title">${category}</div>
-                <div class="navbar-item__btn"><img src="./img/index/arrow-down.png" alt=""></div>
-            </div>
-            <div class="navbar-item-options"></div>
-        </div>
-        `
-    }
-
-    console.log(categoryProperties);
-
-
-
-
-    if (navbarContainer) {
-        navbarContainer.insertAdjacentHTML("beforeend", innerHtmlContainer)
-    }
-
+    
 }
 
-// displayProductsCategories(categories, ".navbar-item__category-options")
-displayProductOptions(categoryProperties, ".filters__navbar")
-
-
-// ACCORDION ITEMS
-const accordionItems = document.querySelectorAll(".navbar-item")
-
-accordionItems.forEach((item, index) => {
-    const title = item.querySelector(".navbar-item__header")
-    const description = item.querySelector(".navbar-item__category-options")
-
-    title.addEventListener("click", () => {
-        item.classList.toggle("open")
-        console.log(description.scrollHeight);
-        if (item.classList.contains("open")) {
-            description.style.height = `${description.scrollHeight}px`
-        } else {
-            description.style.height = `0px`
+// Display all options
+function displayAllFilters(category) {
+    let filterHTML = "";
+    for (const property in categoryProperties[category]) {
+        let properties = ""
+        for (const value of categoryProperties[category][property]) {
+            properties += 
+            `
+                <label class="checkbox-label">
+                    <input type="checkbox" value="${value}" class="checkbox">${value}
+                </label>
+            `
         }
+
+        filterHTML += `
+            <div class="navbar-item">
+                <div class="navbar-item__header">
+                    <div class="navbar-item__title">${property[0].toLocaleUpperCase() + property.replace("_", " ").slice(1)}</div>
+                    <div class="navbar-item__btn"><img src="./img/index/arrow-down.png" alt=""></div>
+                </div>
+                <div class="navbar-item-options accordion-description">${properties}</div>
+            </div>`;
+    }
+    if (navbarContainer) {
+        navbarContainer.insertAdjacentHTML("beforeend", filterHTML);
+    }
+
+    initAccordion();
+}
+
+
+// ACCORDION 
+function initAccordion(){
+    const accordionItems = document.querySelectorAll(".navbar-item")
+    accordionItems.forEach((item, index) => {
+        const title = item.querySelector(".navbar-item__header")
+        const description = item.querySelector(".accordion-description");
+        title.addEventListener("click", () => {
+            item.classList.toggle("open")
+            console.log(description.scrollHeight);
+            if (item.classList.contains("open")) {
+                description.style.height = `${description.scrollHeight}px`
+            } else {
+                description.style.height = `0px`
+            }
+        })
     })
-})
-// -------------------------//
+}
+
+
+function renderProductsThroughFilters(category){
+    const checkboxes = document.querySelectorAll(".checkbox");
+    let checkedFilters = {};
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", () => {
+            const filterType = checkbox.closest(".navbar-item").querySelector(".navbar-item__title").textContent.toLowerCase();
+            const filterValue = checkbox.value;
+
+            // Додаємо або видаляємо значення з фільтрів
+            if (checkbox.checked) {
+                if (!checkedFilters[filterType]) {
+                    checkedFilters[filterType] = [];
+                }
+                if (!checkedFilters[filterType].includes(filterValue)) {
+                    checkedFilters[filterType].push(filterValue);
+                }
+            } else {
+                if (checkedFilters[filterType]) {
+                    checkedFilters[filterType] = checkedFilters[filterType].filter(val => val !== filterValue);
+                    if (checkedFilters[filterType].length === 0) {
+                        delete checkedFilters[filterType];
+                    }
+                }
+            }
+
+            console.log("Filters:", checkedFilters);
+
+            const filteredProducts = products.filter(product => {
+                if (product.category !== category) return false;
+
+                for (const filterType in checkedFilters) {
+                    if (!checkedFilters[filterType].some(value => product[filterType]?.toString() === value)) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+            const productsContainer = document.querySelector(".categories__items");
+            productsContainer.innerHTML = "";
+            productsContainer.classList.remove("empty")
+            
+
+            if (filteredProducts.length === 0) {
+                productsContainer.classList.add("empty")
+                productsContainer.innerHTML = `<div class="no-products">No products found.</div>`;
+                return;
+            }
+
+            filteredProducts.forEach(product => {
+                productsContainer.innerHTML += `
+                    <div class="product__item">
+                        <div class="product__item-wrapper">
+                            <a class="product-info-page-link" href="product-info.html" data-id="${product.id}">
+                                <div class="product__item-image">
+                                    <img src="${product.image}" alt="${product.title}">
+                                </div>
+                                <div class="product__item-name">${product.title}</div>
+                            </a>
+                            <div class="product__item-price">${product.price} ${product.currency}</div>
+                            <div class="product__item-btn small-btn"><a href="#">Buy Now</a></div>
+                        </div>
+                    </div>
+                `;
+            });
+        });
+    });
+}
+// ------------------------------------------------------------------------------------------------
+
+
+renderProducts(".categories__items", 12, true, [], [], [], false, [], 0)
+loadAdditionProducts();
+displayProductsCategories(categoryProperties, ".filters__navbar")
+initAccordion()
